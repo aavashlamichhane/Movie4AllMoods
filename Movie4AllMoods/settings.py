09b2890b44a0dd3607180edb3c5d41fea8d234f2
aavashlamichhane <aavashlamichhane@gmail.com>
2023-04-15 16:13:58 +0545

    Hidden secret key

--- Movie4AllMoods/settings.py
+++ Movie4AllMoods/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1_%0t8d1m^+a0lo&%fp=&ycyeq^8%r9pc+$$#r1el3tt(4bi%g'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
