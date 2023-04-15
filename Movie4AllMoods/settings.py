f6e99e4218cfefb9e22f1fab6aef2eeeac6d51ff
aavashlamichhane <aavashlamichhane@gmail.com>
2023-04-15 15:32:42 +0545

    Hello World

--- Movie4AllMoods/settings.py
+++ Movie4AllMoods/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Movie4AllMoods.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
