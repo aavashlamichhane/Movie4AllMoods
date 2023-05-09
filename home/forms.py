75cf4b28cabf5b12f319aca525c12a17c67bbc69
aavashlamichhane <aavashlamichhane@gmail.com>
2023-05-09 15:50:59 +0545

    errors are managed.

--- home/forms.py
+++ home/forms.py
@@ -6,6 +6,10 @@ from django.forms import ModelForm
 from home.models import Profile
 
 class EditProfileForm(ModelForm):
+    username = forms.CharField(max_length=30,widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    fname = forms.CharField(max_length=50,widget=forms.TextInput(attrs={'class': 'form-control'}))
+    lname = forms.CharField(max_length=50,widget=forms.TextInput(attrs={'class': 'form-control'}))
     class Meta:
         model = User
         fields = (
