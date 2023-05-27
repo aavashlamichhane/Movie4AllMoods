163d90000eb373896dbcf0ad82a9822814d408fb
aavashlamichhane <aavashlamichhane@gmail.com>
2023-05-27 15:05:25 +0545

    trying to fix index

--- home/views.py
+++ home/views.py
@@ -181,76 +181,78 @@ def landing(request):
         return render(request, 'home/landing.html')
  
 def index(request):
-    
-    movie = Movies.objects.all().order_by('-numVotes')[:10000]
-    movies_panda=pd.DataFrame([t.__dict__ for t in movie])
-    features = ['cast']
-    for feature in features:
-        movies_panda[feature]=movies_panda[feature].apply(literal_eval)
-    
-    features = ['cast']
-    for feature in features:
-        movies_panda[feature]=movies_panda[feature].apply(get_list)
-    # print(movies_panda[['title','cast','crew','genre']].head(5))
-    features = ['cast','crew','genre']
-    for feature in features:
-        movies_panda[feature] = movies_panda[feature].apply(clean_data)
-    # print(movies_panda[['title','cast','crew','genre']].head(5))
-    movies_panda['soup']=movies_panda.apply(create_soup,axis=1)
-    # print(movies_panda[['cast','crew','genre','soup']].head(5))
-    count = CountVectorizer(stop_words='english')
-    count_matrix = count.fit_transform(movies_panda['soup'])
-    userlist = list.objects.filter(user=request.user,status=1,rating__gte=6)
-    ranges = range(10,5,-1)
-    list_of_list = []
-    for i in ranges:
-        userlist = list.objects.filter(user=request.user,status=1,rating=i)
-        hajar = []
-        for m in userlist:
-            hajar.append(m.movie.pk)
-        list_of_list.append(hajar)
-    
-    
-    
-    similarity = cosine_similarity(count_matrix,count_matrix)
-    movies_panda = movies_panda.reset_index()
-    indices = pd.Series(movies_panda.index,index=movies_panda['title'])
-    def get_recom(title,number,cosine_sim=similarity):
-        idx = indices[title]
-        sim_scores= builtins.list(enumerate(cosine_sim[idx].tolist()))
-        sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-        sim_scores = sim_scores[1:number+1]
-        movie_indices = [i[0] for i in sim_scores]
-        return movies_panda.iloc[movie_indices]
-    
-    recommended = []
-    ranvar = int(10)
-    for item in list_of_list:
-        if len(item)==0:
-            ranvar-=1
-            continue
-        for item2 in item:
-            to_get = Movies.objects.get(pk=item2).title
-            # print(to_get)
-            no_of_recom = get_no(ranvar,len(item))
-            # print(no_of_recom)
-            if no_of_recom == 0:
-                no_of_recom+=1
-            recomm = get_recom(to_get,no_of_recom)
-            # print(type(recomm))
-            # print(recomm[['id','title']])
-            for entries in recomm['id'].tolist():
-                recommended.append(entries)
-        ranvar-=1
-    movies = []
-    for entry in recommended:
-        if list.objects.filter(user=request.user,movie=Movies.objects.get(pk=entry),status=1).exists():
-            continue
-        else:
-            movies.append(Movies.objects.get(pk=entry))
+    if request.user.is_authenticated:
+        movie = Movies.objects.all().order_by('-numVotes')[:10000]
+        movies_panda=pd.DataFrame([t.__dict__ for t in movie])
+        features = ['cast']
+        for feature in features:
+            movies_panda[feature]=movies_panda[feature].apply(literal_eval)
         
-    if len(movies)==0:
-        movies = Movies.objects.all().order_by('-numVotes')[:20]
+        features = ['cast']
+        for feature in features:
+            movies_panda[feature]=movies_panda[feature].apply(get_list)
+        # print(movies_panda[['title','cast','crew','genre']].head(5))
+        features = ['cast','crew','genre']
+        for feature in features:
+            movies_panda[feature] = movies_panda[feature].apply(clean_data)
+        # print(movies_panda[['title','cast','crew','genre']].head(5))
+        movies_panda['soup']=movies_panda.apply(create_soup,axis=1)
+        # print(movies_panda[['cast','crew','genre','soup']].head(5))
+        count = CountVectorizer(stop_words='english')
+        count_matrix = count.fit_transform(movies_panda['soup'])
+        userlist = list.objects.filter(user=request.user,status=1,rating__gte=6)
+        ranges = range(10,5,-1)
+        list_of_list = []
+        for i in ranges:
+            userlist = list.objects.filter(user=request.user,status=1,rating=i)
+            hajar = []
+            for m in userlist:
+                hajar.append(m.movie.pk)
+            list_of_list.append(hajar)
+        
+        
+        
+        similarity = cosine_similarity(count_matrix,count_matrix)
+        movies_panda = movies_panda.reset_index()
+        indices = pd.Series(movies_panda.index,index=movies_panda['title'])
+        def get_recom(title,number,cosine_sim=similarity):
+            idx = indices[title]
+            sim_scores= builtins.list(enumerate(cosine_sim[idx].tolist()))
+            sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+            sim_scores = sim_scores[1:number+1]
+            movie_indices = [i[0] for i in sim_scores]
+            return movies_panda.iloc[movie_indices]
+        
+        recommended = []
+        ranvar = int(10)
+        for item in list_of_list:
+            if len(item)==0:
+                ranvar-=1
+                continue
+            for item2 in item:
+                to_get = Movies.objects.get(pk=item2).title
+                # print(to_get)
+                no_of_recom = get_no(ranvar,len(item))
+                # print(no_of_recom)
+                if no_of_recom == 0:
+                    no_of_recom+=1
+                recomm = get_recom(to_get,no_of_recom)
+                # print(type(recomm))
+                # print(recomm[['id','title']])
+                for entries in recomm['id'].tolist():
+                    recommended.append(entries)
+            ranvar-=1
+        movies = []
+        for entry in recommended:
+            if list.objects.filter(user=request.user,movie=Movies.objects.get(pk=entry),status=1).exists():
+                continue
+            else:
+                movies.append(Movies.objects.get(pk=entry))
+            
+        if len(movies)==0:
+            movies = Movies.objects.all().order_by('-numVotes')[:20]
+    else:
+        movies=[]
     tmovie=Movies.objects.all().order_by('-imdbscore')[:10]
     pmovie=Movies.objects.all().order_by('-numVotes')[:10]
     lmovie=Movies.objects.all().order_by('-date')[:10]
